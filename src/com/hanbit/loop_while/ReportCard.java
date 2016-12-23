196aa9b65b105ae68b8015b78d20ed2661b2ef41
dpaak2 <dpaak2@naver.com>
2016-12-23 17:40:25 +0900

    As User Write

---
+++ src/com/hanbit/loop_while/ReportCard.java
@@ -0,0 +1,40 @@
+package com.hanbit.loop_while;
+
+import java.util.Scanner;
+
+public class ReportCard {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String name = "";
+		int score = 0, total = 0, average = 0, sub = 0;
+		System.out.println("If you want to stop,Type -1");
+		System.out.print("Name:");
+		name = s.next();
+		while (true) {
+			System.out.print("Score=");
+			score = s.nextInt();
+			sub = sub + 1;
+			if (score == -1) {
+				char grade = '0';
+				if (average >= 90) {
+					grade = 'A';
+				} else if (average >= 80) {
+					grade = 'B';
+				} else if (average >= 70) {
+					grade = 'C';
+				} else if (average >= 60) {
+					grade = 'D';
+				} else {
+					grade = 'F';
+				}
+				System.out.println("Name:" + name);
+				System.out.println("Total=" + total);
+				System.out.println("Average=" + average);
+				System.out.println("Grade " + grade);
+				break;
+			}
+			total = total + score;
+			average = (total) / sub;
+		}
+	}
+}
