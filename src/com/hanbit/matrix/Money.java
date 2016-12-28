79a3116925fd289b37560667c4760528f3ca5352
dpaak2 <dpaak2@naver.com>
2016-12-28 20:26:47 +0900

    As User Write

--- src/com/hanbit/matrix/Money.java
+++ src/com/hanbit/matrix/Money.java
@@ -10,71 +10,58 @@ public class Money {
 		
 		System.out.println("금액을 입력하세요. ");
 		money =s.nextInt();
-		System.out.println(money+"원");
+		System.out.println("요청금액: "+money);
 		
 		int [] moneyUnit ={50000,10000,5000,1000,500,100,50,10};
-		
+		System.out.println("요청금액: "+money);
 			int mok =0;
 			int nmg =0;
-			System.out.println("Money:"+money);
+			String amount= "";
 			for(i=0;i<moneyUnit.length;i++){
-				switch(moneyUnit[i]){
-				
-				case 1:
-					money=money%moneyUnit[i];
-					mok=money/50000;
-					nmg=money%50000;
-					nmg=money;
+				switch(i){
+				case 0:
+					mok=money/moneyUnit[i];
+					
+					nmg=money%moneyUnit[i];
 				break;
-			
+				case 1:
+					mok=money/moneyUnit[i];
+					money=nmg;
+					nmg=money%moneyUnit[i];
+					break;
 				case 2:
-					money=money%moneyUnit[i];
-					mok=money%10000;
-					nmg=mok%10000;
+					mok=money/moneyUnit[i];
+					money=nmg;
+					nmg=money%moneyUnit[i];
 					break;
 				case 3:
-					money=money%moneyUnit[i];
-					mok=money%5000;
-					nmg=mok%5000;
+					mok=money/moneyUnit[i];
+					money=nmg;;
+					nmg=money%moneyUnit[i];
 					break;
 				case 4:
-					money=money%moneyUnit[i];
-					mok=money%1000;
-					nmg=mok%1000;
+					mok=money/moneyUnit[i];
+					money=nmg;
+					nmg=money%moneyUnit[i];
 					break;
 				case 5:
-					money=money%moneyUnit[i];
-					mok=money%500;
-					nmg=mok%500;
+					mok=money/moneyUnit[i];
+					money=nmg;
+					nmg=money%moneyUnit[i];
 					break;
 				case 6:
-					money=money%moneyUnit[i];
-					mok=money%100;
-					nmg=mok%100;
+					mok=money/moneyUnit[i];
+					money=nmg;
+					nmg=money%moneyUnit[i];
 					break;
 				case 7:
-					money=money%moneyUnit[i];
-					mok=money%50;
-					nmg=mok%50;
+					mok=money/moneyUnit[i];
+					money=nmg;
+					nmg=money%moneyUnit[i];
 					break;
-				case 8:
-					money=money%moneyUnit[i];
-					mok=money%10;
-					nmg=mok%10;
-						break;
-				default:
-					money=money%moneyUnit[i];
-					break;
-				}
+				}amount+=moneyUnit[i]+"\t"+mok+"개";
 			}
-		System.out.println("5만원 :"+mok);
-		System.out.println("1만원 :");
-		System.out.println("5천원 :");
-		System.out.println("1천원 :");
-		System.out.println("500원 :");
-		System.out.println("100원 :");
-		System.out.println("50원 :");
-		System.out.println("10원 :");
+			System.out.println(amount);
 	}
 
 }
